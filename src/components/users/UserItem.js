bb68021cd233f71511cbf478a23af158d70bccfa
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-16 00:22:58 -0400

    User page setup

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
@@ -12,14 +13,12 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a
-          href={html_url}
+        <Link
+          to={`/user/${login}`}
           className='btn btn-dark btn-sm my-1'
-          target='_blank'
-          rel='noreferrer'
         >
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
