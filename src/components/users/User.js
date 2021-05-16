cca7aacd6053db89c8f463f6dbd16285cc08daa6
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-16 09:03:01 -0400

    functionality done

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -68,7 +68,7 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
-            <a href={html_url} className='btn btn-dark my-1'>
+            <a href={html_url} className='btn btn-dark my-1' target='_blank' rel="noreferrer">
               Visit Github Profile
             </a>
             <ul>
